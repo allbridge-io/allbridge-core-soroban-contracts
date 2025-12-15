6209a92975556e3a2192e82dbc2b83282321439f
faramozzayw <faramo_zayw@protonmail.com>
2025-12-15 14:32:13 +0200

    WIP

--- contracts/auto-deposit-wallet/src/methods/internal/swap_and_bridge.rs
+++ contracts/auto-deposit-wallet/src/methods/internal/swap_and_bridge.rs
@@ -1,7 +1,88 @@
-use shared::Error;
-use soroban_sdk::{Address, Env};
+use shared::{
+    utils::{address_to_bytes, safe_cast},
+    Error,
+};
+use soroban_sdk::{
+    auth::{ContractContext, InvokerContractAuthEntry, SubContractInvocation},
+    vec, Address, Env, IntoVal, Symbol, U256,
+};
 
 use crate::{bridge, gas_oracle, messenger, storage::config::Config};
+
+pub fn swap_and_bridge(
+    env: &Env,
+    config: &Config,
+    token: &Address,
+    amount: u128,
+    nonce: &U256,
+) -> Result<(), Error> {
+    let bridge_client = bridge::Client::new(env, &config.bridge);
+    let cost_in_tokens = get_bridging_cost_in_tokens(env, config, token, &bridge_client)? + 1;
+    let amount_without_fee = amount - cost_in_tokens;
+
+    let pool = bridge_client
+        .get_config()
+        .pools
+        .get(address_to_bytes(&env, &token)?)
+        .ok_or(Error::NotFound)?;
+
+    env.authorize_as_current_contract(vec![
+        env,
+        InvokerContractAuthEntry::Contract(SubContractInvocation {
+            context: ContractContext {
+                contract: token.clone(),
+                fn_name: Symbol::new(&env, "transfer"),
+                args: vec![
+                    env,
+                    env.current_contract_address().to_val(),
+                    config.bridge.to_val(),
+                    safe_cast::<_, i128>(cost_in_tokens)?.into_val(env),
+                ],
+            },
+            sub_invocations: vec![&env],
+        }),
+        InvokerContractAuthEntry::Contract(SubContractInvocation {
+            context: ContractContext {
+                contract: token.clone(),
+                fn_name: Symbol::new(&env, "transfer"),
+                args: vec![
+                    env,
+                    env.current_contract_address().to_val(),
+                    pool.to_val(),
+                    safe_cast::<_, i128>(amount_without_fee)?.into_val(env),
+                ],
+            },
+            sub_invocations: vec![&env],
+        }),
+        InvokerContractAuthEntry::Contract(SubContractInvocation {
+            context: ContractContext {
+                contract: pool.clone(),
+                fn_name: Symbol::new(&env, "swap_to_v_usd"),
+                args: vec![
+                    env,
+                    env.current_contract_address().to_val(),
+                    amount_without_fee.into_val(env),
+                    false.into_val(env),
+                ],
+            },
+            sub_invocations: vec![env],
+        }),
+    ]);
+
+    bridge_client.swap_and_bridge(
+        &env.current_contract_address(),
+        token,
+        &amount,
+        &config.recipient,
+        &config.recipient_chain_id,
+        &config.recipient_token,
+        nonce,
+        &0,
+        &cost_in_tokens,
+    );
+
+    Ok(())
+}
 
 pub fn get_bridging_cost_in_tokens(
     env: &Env,
--- contracts/auto-deposit-wallet/src/methods/public/factory_swap_and_bridge.rs
+++ contracts/auto-deposit-wallet/src/methods/public/factory_swap_and_bridge.rs
@@ -1,13 +1,6 @@
-use crate::{bridge, methods::internal::get_bridging_cost_in_tokens, storage::config::Config};
-use shared::{
-    soroban_data::SimpleSorobanData,
-    utils::{address_to_bytes, safe_cast},
-    Error,
-};
-use soroban_sdk::{
-    auth::{ContractContext, InvokerContractAuthEntry, SubContractInvocation},
-    vec, Address, Env, IntoVal as _, Symbol, U256,
-};
+use crate::{methods::internal::swap_and_bridge, storage::config::Config};
+use shared::{soroban_data::SimpleSorobanData, Error};
+use soroban_sdk::{Address, Env, U256};
 
 pub fn factory_swap_and_bridge(
     env: Env,
@@ -18,57 +11,5 @@ pub fn factory_swap_and_bridge(
     let config = Config::get(&env)?;
     config.factory.require_auth();
 
-    let bridge_client = bridge::Client::new(&env, &config.bridge);
-    let pool = bridge_client
-        .get_config()
-        .pools
-        .get(address_to_bytes(&env, &token_address)?)
-        .ok_or(Error::NotFound)?;
-
-    let cost_in_tokens =
-        get_bridging_cost_in_tokens(&env, &config, &token_address, &bridge_client)? + 1;
-
-    env.authorize_as_current_contract(vec![
-        &env,
-        InvokerContractAuthEntry::Contract(SubContractInvocation {
-            context: ContractContext {
-                contract: token_address.clone(),
-                fn_name: Symbol::new(&env, "transfer"),
-                args: vec![
-                    &env,
-                    env.current_contract_address().to_val(),
-                    config.bridge.to_val(),
-                    safe_cast::<_, i128>(cost_in_tokens)?.into_val(&env),
-                ],
-            },
-            sub_invocations: vec![&env],
-        }),
-        InvokerContractAuthEntry::Contract(SubContractInvocation {
-            context: ContractContext {
-                contract: token_address.clone(),
-                fn_name: Symbol::new(&env, "transfer"),
-                args: vec![
-                    &env,
-                    env.current_contract_address().to_val(),
-                    pool.to_val(),
-                    safe_cast::<_, i128>(amount - cost_in_tokens)?.into_val(&env),
-                ],
-            },
-            sub_invocations: vec![&env],
-        }),
-    ]);
-
-    bridge_client.swap_and_bridge(
-        &env.current_contract_address(),
-        &token_address,
-        &amount,
-        &config.recipient,
-        &config.recipient_chain_id,
-        &config.recipient_token,
-        &nonce,
-        &0,
-        &cost_in_tokens,
-    );
-
-    Ok(())
+    swap_and_bridge(&env, &config, &token_address, amount, &nonce)
 }
